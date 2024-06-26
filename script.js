aa0f4701413554e01364dff7f1d9d433ef547dfd
Raushan365 <raushansingh0427@gmail.com>
2024-06-26 20:49:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const newTodoInput = document.getElementById('newTodo');
+const addBtn = document.getElementById('addBtn')
+const todoList = document.getElementById('todoList')
+function addTodo(){
+    const newTodoText = newTodoInput.value;
+    newTodoInput.value = '';
+
+    const listItem = document.createElement('li');
+    const completeCheckbox = document.createElement('input');
+    const todoText = document.createElement('span')
+    const deleteBtn = document.createElement('button')
+
+    completeCheckbox.type = 'checkbox';
+    listItem.appendChild(completeCheckbox);
+    listItem.appendChild(todoText);
+    listItem.appendChild(deleteBtn);
+
+    todoText.textContent = newTodoText;
+    deleteBtn.textContent = 'Delete';
+    todoList.appendChild(listItem);
+}
+
+addBtn.addEventListener('click',addTodo)
